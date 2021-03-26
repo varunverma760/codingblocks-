1bf553be5ab785c3b8ea12e70a616df4da5a4f27
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-03-26 11:48:35 +0530

    Add files via upload

---
+++ pointersandarray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a[]={1,2,3,4,5};
+
+cout<<"Adress of a is "<<a<<endl;
+cout<<"Adress of a[0] is "<<&a[0]<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+
+
+
+
+
+
+}
+
+
