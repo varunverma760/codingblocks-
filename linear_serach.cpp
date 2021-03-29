3e31b878d5dd55b972fba09ae6fa525659bf311d
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-03-29 12:53:59 +0530

    Update linear_serach.cpp

--- linear_serach.cpp
+++ linear_serach.cpp
@@ -1,31 +1,31 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
-int main(){
-	int arr[100];
-	cout<<"enter the number of elements :"<<endl;
 
-	int n,key;
-	cin>>n;
-	for(int i=0;i<n;i+++)
-	{
-		cin>>arr[i];
-	}
-	cout<<"enter key"<<endl;
-	cin>>key;
+int main()
+{
+   int arr[100];
+   int i=0,n,key;
+   cin>>n;
+   for(i=0;i<n;i++){
+       cin>>arr[i];
+   }
+   cout<<"Enter the value of key"<<endl;
+   cin>>key;
+   for(i=0;i<n;i++){
+       if (key==arr[i]){
+           cout<<"Element found at the index :"<<i<<endl;
+           break;
+       }
+       
+   }
+    cout << i;
+    cout<<"i"<<endl;
+    if( i==n){
+       cout<<"Element not found"<<endl;
+        }
+       
+  
 
-	for(int i=0;i<n;i++){
-		if(key==arr[i]){
-			cout<<"element is foud at index :"<<i<<endl;
-			break;
-		}
-	}
-	
-
-	if (i==n){
-		cout<<"key is not present"<<endl; 
-	}
-
-
-
-	return 0;
+    return 0;
 }
