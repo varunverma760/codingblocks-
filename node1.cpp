56103eec5bf54fb2a1be5a2365feaffd938a15d3
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-03-29 19:02:56 +0530

    Add files via upload

---
+++ node1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class Node{
+public:
+	int data;
+	Node*next;
+
+Node(int data){
+	this -> data =data;
+	next=NULL;
+  }
+};
+int main(){
+	
+// statically	
+	Node n1(1);
+
+	Node*head = &n1;
+
+
+	Node n2(2);
+	
+	n1.next = &n2;
+
+	cout<<head->data<<endl;
+
+
+
+	return 0;
+
+
+}
