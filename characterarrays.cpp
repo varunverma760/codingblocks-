6dcb9f899fddb2bfdbb64c6d1d1b1345bb341b7e
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-04-14 21:11:31 +0530

    Create characterarrays.cpp

---
+++ characterarrays.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	
+	//intialization
+	char arr[]={'A','b','c','D'};
+	char arr1[]={'A','b','c','D'};
+	cout<<arr<<endl;
+	cout<<arr1<<endl;
+	
+	
+	
+	
+	
+	return 0;
+	
+}
