f0456a5b324501c873bbb0acc8ae6af82b911b7e
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-04-15 00:39:25 +0530

    Create reverse3.cpp

---
+++ reverse3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int length(char *a){
+	int i=0;
+	int co=0;
+	while (a[i]!='\0'){
+		co++;
+		i++;
+	}
+	return co;
+	
+}
+void reverse(char *a){
+	int i=0;
+	int j=length(a)-1;
+	while(i<j){
+		swap(a[i],a[j]);
+		i++;
+		j--; 
+	}
+}
+int main(){
+	
+	char a[100];
+	cin>>a;
+	
+
+	cout<<"Before reversing "<<" "<<a<<endl;
+	reverse(a);
+	
+	cout<<"after reversing"<<" "<<a<<endl;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
