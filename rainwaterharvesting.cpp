7390a92b76942b88a9ce6206d5441426f1a8d2db
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-04-17 00:48:34 +0530

    Create rainwaterharvesting.cpp

---
+++ rainwaterharvesting.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int height[1000005],leftmax[1000005],rightmax[1000005];
+int main(){
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>height[i];
+	}
+	leftmax[0]=height[0];
+	for(int i=1;i<n;i++)	{
+		leftmax[i]=max(height[i],leftmax[i-1]);
+	}
+	rightmax[n-1]=height[n-1];
+	for(int i=n-2;i>=0;i--){
+		rightmax[i]=max(height[i],rightmax[i+1]);
+	}
+	int water=0;
+	for(int i=0;i<n;i++){
+		min(rightmax[i],leftmax[i]-height[i]);
+		
+	}
+	cout<<water<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+}
