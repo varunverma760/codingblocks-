e337f7e05062e67c1e6e6344f6ed6f5e763869ba
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-04-16 14:05:01 +0530

    Create brokencalculator.cpp

---
+++ brokencalculator.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+void multiply(int*a,int &n,int no){
+	int carry=0;
+	for(int i=0;i<n;i++){
+		int product =a[i]*no+carry;
+		a[i]=product%10;
+		carry=product/10;
+		
+		
+	} 
+	while(carry!=0){
+		a[n]=carry%10;
+		carry=carry/10;
+		n++;
+	} 
+	
+	
+	
+}
+void factorial(int num){
+	int a[1000];
+	for(int i=0;i<1000;i++){
+		a[i]=0;
+	}
+	
+	a[0]=1;
+	int n=1;
+	for(int i=2;i<=num;i++){
+		multiply(a,n,i);
+	}
+	for(int i=n-1;i>=0;i++){
+		cout<<a[i];
+		
+	}
+	cout<<endl;
+}
+int main(){
+	int n;
+	cin>>n;
+	factorial(n);
+	
+	
+	
+	
+	return 0;
+}
