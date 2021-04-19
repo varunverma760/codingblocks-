ba15f4c17089512699cfe01babb01bdc6a1496d3
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-04-19 21:21:35 +0530

    Create wavprintin2darray.cpp

---
+++ wavprintin2darray.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int arr[100][100];
+	int rows,cols;
+	cin>>rows>>cols;
+	for(int i=0;i<rows;i++){
+		for(int j=0;j<cols;j++){
+			cin>>arr[i][j];
+		}
+	}
+	
+	//PRINTING ARRAY
+	for(int i=0;i<rows;i++){
+		for(int j=0;j<cols;j++){
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	for(int i=0;i<cols;i++){
+		if(i%2==0){
+			//MEANS EVEN
+			
+			for(int j=0;j<rows;j++){
+				cout<<arr[j][i];
+			}
+			
+		}
+		else {
+			//MEANS ODD
+			
+			for(int j=rows-1;j>=0;j--){
+				cout<<arr[j][i]<<" ";
+			}
+		}
+	}
+	cout<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
