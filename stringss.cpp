204053408e19cd6bafee1d9bb4e1963e6c7a2ba8
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-04-19 21:35:18 +0530

    Create stringss.cpp

---
+++ stringss.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+	
+	string s;
+	//cin>>s;
+	//getline(cin,stringname);
+	getline(cin,s);
+	cout<<s<<endl;
+	
+	
+	
+	
+	
+	return 0;
+}
