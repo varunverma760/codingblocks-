0013346f9504cc664d7932d4cfe5a8ac13f7e83f
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-04-19 21:40:56 +0530

    Update stringss.cpp

--- stringss.cpp
+++ stringss.cpp
@@ -8,6 +8,18 @@ int main(){
 	//getline(cin,stringname);
 	getline(cin,s);
 	cout<<s<<endl;
+	//2nd way
+	string s1="coding blocks";
+	cout<<s1<<endl;
+	//3rd way
+	string s2("i am michael jordan :-)");
+	cout<<s2<<endl;
+	//4th way
+	char a[]="i live in new delhi";
+	cout<<a<<endl;
+	
+	string s4(a);
+	cout<<s4<<endl;
 	
 	
 	
