4984dc464ec39ad8da295a6ac0910a031e65f610
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-04-20 23:45:45 +0530

    Update binarysearch2.cpp

--- binarysearch2.cpp
+++ binarysearch2.cpp
@@ -1,29 +1,44 @@
-#include <iostream>
-using namespace std;
+/******************************************************************************
 
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
 int binarysearch(int a[],int n,int key){
+    
     int s=0;
     int e=n-1;
-
     while(s<=e){
-         int mid=(s+e)/2;
-
-    if(a[mid]==key){
-        return mid;
-
+        int mid=(s+e)/2;
+        
+        if(a[mid]==key){
+            
+            return mid;
+                
+        }
+        if(a[mid]>key){
+            e=mid-1;
+        }
+        else{
+            
+            s=mid+1;
+        }
+    
+        
+        
+        
     }
-    else if(a[mid]>key){
-        e=mid-1;
-    }
-    else{
-        s=mid+1;
-    }
-
-    }
-   
-
+    
+    
 }
-int main() {
+
+int main()
+{
     int a[100];
     int n;
     cin>>n;
@@ -31,11 +46,11 @@ int main() {
         cin>>a[i];
     }
     int key;
-    cout<<"enter key"<<endl;
+    cout<<"Enter the value  of key"<<endl;
     cin>>key;
-    binarysearch(a,n,key);
+    
+    int x=binarysearch(a,n,key);
+    cout<<"key is found"<<x<<endl;
 
-
-
+    return 0;
 }
-
